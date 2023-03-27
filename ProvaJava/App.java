7404ec0330747b95891b727ed5e627baddee9b21
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-03-27 14:45:00 -0300

    27.03

--- ProvaJava/App.java
+++ ProvaJava/App.java
@@ -3,6 +3,8 @@ package ProvaJava;
 public class App {
     public static void main(String[] args) {
         Atividade obj = new Atividade();
+        obj. exercicio1();
+        obj. exercicio2();
         obj. exercicio3();
         
     }
